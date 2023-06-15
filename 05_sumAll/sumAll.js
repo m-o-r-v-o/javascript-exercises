006641d5cf7fa1c116fe2ee4b651ba19843dbbb1
keith <8584184+m-o-r-v-o@users.noreply.github.com>
2023-06-15 11:33:53 -0500

    Solves 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != 'number' || typeof(b) != 'number' || a < 0 || b < 0){
+        return 'ERROR';
+    }
+    let smallNumber = 0;
+    let largeNumber = 0;
+    let sum = 0;
+    if (a > b){
+        smallNumber = b;
+        largeNumber = a;
+    }
+    else {
+        smallNumber = a;
+        largeNumber = b;
+    }
 
+    for (i = smallNumber; i <= largeNumber; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
